53681833a4b9416ae0bdba82311848d0525dd773
MILLKA3000 <s.tchekanov@svitla.com>
2016-05-05 15:26:55 +0300

    fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,5 @@
-@extends('layouts.app')
-
-{{-- Web site Title --}}
-@section('title') {!!  trans('site/user.login') !!} :: @parent @stop
-
-{{-- Content --}}
+@extends('layouts.app') {{-- Web site Title --}} @section('title') {!!  
+trans('site/user.login') !!} :: @parent @stop {{-- Content --}} 
 @section('content')
     <div class="row">
         <div class="page-header">
@@ -13,21 +9,30 @@
     <?= (isset($google_err)) ? $google_err : '' ?>
     <div class="container-fluid">
         <div class="row">
-            {!! Form::open(array('url' => URL::to('auth/login'), 'method' => 'post', 'files'=> true)) !!}
-            <div class="col-md-offset-2 form-group  {{ $errors->has('email') ? 'has-error' : '' }}">
-                {!! Form::label('email', "E-Mail Address", array('class' => 'control-label col-md-2')) !!}
+            {!! Form::open(array('url' => URL::to('auth/login'), 
+'method' => 'post', 'files'=> true)) !!}
+            <div class="col-md-offset-2 form-group {{ 
+$errors->has('email') ? 'has-error' : '' }}">
+                {!! Form::label('email', "E-Mail Address", array('class' 
+=> 'control-label col-md-2')) !!}
                 <div class="controls col-md-4">
-                    {!! Form::text('email', null, array('class' => 'form-control')) !!}
-                    <span class="help-block">{{ $errors->first('email', ':message') }}</span>
+                    {!! Form::text('email', null, array('class' => 
+'form-control')) !!}
+                    <span class="help-block">{{ $errors->first('email', 
+':message') }}</span>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-offset-2 form-group  {{ $errors->has('password') ? 'has-error' : '' }}">
-                {!! Form::label('password', "Password", array('class' => 'control-label col-md-2')) !!}
+            <div class="col-md-offset-2 form-group {{ 
+$errors->has('password') ? 'has-error' : '' }}">
+                {!! Form::label('password', "Password", array('class' => 
+'control-label col-md-2')) !!}
                 <div class="controls col-md-4">
-                    {!! Form::password('password', array('class' => 'form-control')) !!}
-                    <span class="help-block">{{ $errors->first('password', ':message') }}</span>
+                    {!! Form::password('password', array('class' => 
+'form-control')) !!}
+                    <span class="help-block">{{ 
+$errors->first('password', ':message') }}</span>
                 </div>
             </div>
         </div>
@@ -35,13 +40,14 @@
             <div class="form-group">
                 <div class="col-md-offset-4">
                     <div class="checkbox">
-                        <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+                        <button type="submit" class="btn btn-primary" 
+style="margin-right: 15px;">
                             Login
                         </button>
                         <label>
-                            <input type="checkbox" name="remember"> Remember Me
+                            <input type="checkbox" name="remember"> 
+Remember Me
                         </label>
-
                     </div>
                 </div>
             </div>
@@ -56,11 +62,10 @@
         <div class="row">
             <div class="form-group">
                 <div class="col-md-4 col-md-offset-4">
-                    <a class="btn btn-primary" href="{{ URL::to('/googleOauth2') }}"><i></i>Sign in with Google</a>
+                    <a class="btn btn-primary" href="{{ 
+URL::to('/googleOauth2') }}"><i></i>Sign in with Google</a>
                 </div>
             </div>
-
             {!! Form::close() !!}
         </div>
-    </div>
-@endsection
+    </div> @endsection
